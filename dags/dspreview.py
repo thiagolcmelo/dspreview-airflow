d6935a32ebe64969b515e64148dcd0e415dc1301
Thiago Melo <thiago.lc.melo@gmail.com>
2018-08-26 18:02:58 +0000

    scripts for using celery

--- dags/dspreview.py
+++ dags/dspreview.py
@@ -16,8 +16,30 @@ dag_args = {
 }
 
 dag = DAG('dspreview', default_args=dag_args,
-          schedule_interval=timedelta(minutes=30), dagrun_timeout=timedelta(minutes=3))
+          schedule_interval="@hourly", dagrun_timeout=timedelta(minutes=3))
 
-run_dcm_task = BashOperator(task_id='update_dspreview',
-               bash_command='/home/airflow/worker/update.sh ',
+dcm_task = BashOperator(task_id='update_dspreview_dcm',
+               bash_command='/home/airflow/worker/update.sh "dcm" ',
                dag=dag)
+
+dbm_task = BashOperator(task_id='update_dspreview_dbm',
+               bash_command='/home/airflow/worker/update.sh "dsp.dbm" ',
+               dag=dag)
+
+mediamath_task = BashOperator(task_id='update_dspreview_mediamath',
+               bash_command='/home/airflow/worker/update.sh "dsp.mediamath" ',
+               dag=dag)
+
+
+report_task = BashOperator(task_id='update_dspreview_report',
+               bash_command='/home/airflow/worker/update.sh "report" ',
+               dag=dag)
+
+# in this case, it will work because there will be a single worker
+# that handle tasks sequentially, we need to figure out a way to truly
+# grantee that these tasks succeeded prior to generating the report
+# it would allow us to work in a more distributed manner, but for now
+# it will work
+report_task.set_upstream(dcm_task)
+report_task.set_upstream(dbm_task)
+report_task.set_upstream(mediamath_task)
--- worker/update.sh
+++ worker/update.sh
@@ -5,25 +5,51 @@
 # ACTIVATE ENV
 source /home/airflow/worker/worker-env3/bin/activate
 
-# POKE DCM
-dspreview --poke "dcm"
+# if there is a input param
+if [ $# -ge 1 ];
+then
+    # dspreview --poke "${1}"
+    for i in $(printf "a='${1}'.split('.'); print(' '.join(a))" | python)
+    do
+        if [ $i == 'dcm' ]
+        then
+            dspreview --worker dcm
+        elif [ $i == 'dbm' ]
+        then
+            dspreview --worker dsp --dsp dbm
+        elif [ $i == 'mediamath' ]
+        then
+            dspreview --worker dsp --dsp mediamath
+        elif [ $i == 'report' ]
+        then
+            dspreview --generate-report
+        fi
+    done
+else
+    # POKE DCM
+    # dspreview --poke "dcm"
+    dspreview --worker dcm
 
-# SLEEP 10 seconds
-sleep 10
+    # SLEEP 10 seconds
+    sleep 10
 
-# POKE DSP DBM
-dspreview --poke "dsp.dbm"
+    # POKE DSP DBM
+    # dspreview --poke "dsp.dbm"
+    dspreview --worker dsp --dsp dbm
 
-# SLEEP 10 seconds
-sleep 10
+    # SLEEP 10 seconds
+    sleep 10
 
-# POKE DSP MEDIAMATH
-dspreview --poke "dsp.mediamath"
+    # POKE DSP MEDIAMATH
+    # dspreview --poke "dsp.mediamath"
+    dspreview --worker dsp --dsp mediamath
 
-# SLEEP 10 seconds
-sleep 10
+    # SLEEP 10 seconds
+    sleep 10
 
-# GENERATE REPORT
-dspreview --poke "report"
+    # GENERATE REPORT
+    # dspreview --poke "report"
+    dspreview --generate-report
 
-echo "done"
+    echo "done"
+fi;
